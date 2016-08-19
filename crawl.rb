d25c1fec54287dd9db55bc85ceb307718285b8ce
Kiba <hackerkiba@gmail.com>
2016-08-19 17:35:21 -0400

    add variable start_url

--- crawl.rb
+++ crawl.rb
@@ -3,4 +3,6 @@ require "mechanize"
 
 agent = Mechanize.new()
 
-page = agent.get('https://forums.sufficientvelocity.com/threads/marked-for-death-a-rational-naruto-quest.24481/')
+start_url = "https://forums.sufficientvelocity.com/threads/marked-for-death-a-rational-naruto-quest.24481/"
+
+page = agent.get(start_url)
