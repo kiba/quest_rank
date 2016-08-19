f2c32b67910a9271869cdd889ff5b8d64916260c
Kiba <hackerkiba@gmail.com>
2016-08-19 18:32:12 -0400

    limit it to a conservative ten second

--- crawl.rb
+++ crawl.rb
@@ -3,9 +3,12 @@ require "mechanize"
 files = Dir["cache/*.html"]
 
 agent = Mechanize.new()
+agent.history_added = Proc.new {sleep 10}
 
 start_url = "https://forums.sufficientvelocity.com/threads/marked-for-death-a-rational-naruto-quest.24481/"
 
 page = agent.get(start_url)
 
 last = page.css("div.PageNav").first["data-last"].to_i
+
+#messages = page.css("ol")
