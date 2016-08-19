0f2d647c6a1e3982bc60a2b9ec2c7a17a395a825
Kiba <hackerkiba@gmail.com>
2016-08-19 17:28:18 -0400

    get a page

--- crawl.rb
+++ crawl.rb
@@ -1,4 +1,6 @@
 require "mechanize"
-require "logger"
+
 
 agent = Mechanize.new()
+
+page = agent.get('https://forums.sufficientvelocity.com/threads/marked-for-death-a-rational-naruto-quest.24481/')
