40eea751565e9f14a5e37c68259494671f341478
Kiba <hackerkiba@gmail.com>
2016-08-19 18:43:15 -0400

    clean up calculate_size method

--- crawl.rb
+++ crawl.rb
@@ -7,10 +7,10 @@ class Crawl
     @files = Dir["cache/*.html"]
   end
 
-  def calculate_size agent
+  def calculate_size
     start_url = "https://forums.sufficientvelocity.com/threads/marked-for-death-a-rational-naruto-quest.24481/"
+    page = @agent.get(start_url)
     last = page.css("div.PageNav").first["data-last"].to_i
-    page = agent.get(start_url)
     messages = page.css("ol.messageList li.message")
   end
 end
