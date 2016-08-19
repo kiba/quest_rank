7504b1753b359ade9cb212e99f18a7f53f5e3d3a
Kiba <hackerkiba@gmail.com>
2016-08-19 18:04:34 -0400

    figure out how many pages are there to process

--- crawl.rb
+++ crawl.rb
@@ -6,3 +6,5 @@ agent = Mechanize.new()
 start_url = "https://forums.sufficientvelocity.com/threads/marked-for-death-a-rational-naruto-quest.24481/"
 
 page = agent.get(start_url)
+
+last = page.css("div.PageNav").first["data-last"].to_i
