ff4aa0f16f6af5a550d826e53eba4847d3e5edc4
Kiba <hackerkiba@gmail.com>
2016-08-19 18:41:05 -0400

    start organizing code

--- crawl.rb
+++ crawl.rb
@@ -5,11 +5,12 @@ files = Dir["cache/*.html"]
 agent = Mechanize.new()
 agent.history_added = Proc.new {sleep 10}
 
-start_url = "https://forums.sufficientvelocity.com/threads/marked-for-death-a-rational-naruto-quest.24481/"
 
-page = agent.get(start_url)
 
-last = page.css("div.PageNav").first["data-last"].to_i
 
-messages = page.css("ol.messageList li.message")
-puts messages.size
+def calculate_size agent
+  start_url = "https://forums.sufficientvelocity.com/threads/marked-for-death-a-rational-naruto-quest.24481/"
+  last = page.css("div.PageNav").first["data-last"].to_i
+  page = agent.get(start_url)
+  messages = page.css("ol.messageList li.message")
+end
