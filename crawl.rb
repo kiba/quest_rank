6ced55acb299b04bebd414be8a9d63a29a90ab53
Kiba <hackerkiba@gmail.com>
2016-08-19 21:52:04 -0400

    trying to figure out what needed downloading

--- crawl.rb
+++ crawl.rb
@@ -7,8 +7,19 @@ class Crawl
     @files = Dir["cache/*.html"]
   end
   def download
+    url = "https://forums.sufficientvelocity.com/threads/marked-for-death-a-rational-naruto-quest.24481/"
+    start = @files.size
+    if @files.size == 0
+      start = 1
+    end
+    page = @agent.get(url + "page-#{start}")
+    last = page.css("div.PageNav").first["data-last"].to_i - 1
+    (start..last).to_a.each do |n|
+      puts n
+    end
   end
 end
 
 crawl = Crawl.new()
+crawl.download()
 #    messages = page.css("ol.messageList li.message")
