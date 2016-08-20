4f5baa8414537187dbd3b5e5e4f3a1fab22c2bf9
Kiba <hackerkiba@gmail.com>
2016-08-19 21:32:53 -0400

    revise calculation

--- crawl.rb
+++ crawl.rb
@@ -11,7 +11,7 @@ class Crawl
     start_url = "https://forums.sufficientvelocity.com/threads/marked-for-death-a-rational-naruto-quest.24481/"
     page = @agent.get(start_url)
     last = page.css("div.PageNav").first["data-last"].to_i
-    @files.size - last
+    last - @files.size
   end
 end
 
