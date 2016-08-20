88e22e649e2155060537c035668665297001fa3b
Kiba <hackerkiba@gmail.com>
2016-08-19 21:39:54 -0400

    throw out code

--- crawl.rb
+++ crawl.rb
@@ -6,16 +6,9 @@ class Crawl
     @agent.history_added = Proc.new {sleep 10}
     @files = Dir["cache/*.html"]
   end
-
-  def calculate_missing
-    start_url = "https://forums.sufficientvelocity.com/threads/marked-for-death-a-rational-naruto-quest.24481/"
-    page = @agent.get(start_url)
-    last = page.css("div.PageNav").first["data-last"].to_i
-    last - @files.size
+  def download
   end
 end
 
 crawl = Crawl.new()
-puts "Files missing from cache:" + crawl.calculate_missing().to_s
-
 #    messages = page.css("ol.messageList li.message")
