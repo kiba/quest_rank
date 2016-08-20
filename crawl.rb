0da4f0d183f70966e6998842fe5189cfb672a156
Kiba <hackerkiba@gmail.com>
2016-08-20 16:51:40 -0400

    made a lot of changes and adjustment to make it work

--- crawl.rb
+++ crawl.rb
@@ -1,28 +1,29 @@
 require "mechanize"
+URL = "https://forums.sufficientvelocity.com/threads/marked-for-death-a-rational-naruto-quest.24481/"
 
 class Crawl
   def initialize
     @agent = Mechanize.new()
-    @agent.history_added = Proc.new {sleep 10}
+    @agent.history_added = Proc.new {sleep 3}
     @files = Dir["cache/*.html"]
   end
   def target_range
-    url = "https://forums.sufficientvelocity.com/threads/marked-for-death-a-rational-naruto-quest.24481/"
     start = @files.size + 1
-    page = @agent.get(url + "page-#{start}")
+    page = @agent.get(URL + "page-#{start}")
     messages = page.css("ol.messageList li.message")
     return [] unless messages.size == 25
     last = page.css("div.PageNav").first["data-last"].to_i - 1
-    (start..3).to_a.each
+    (start..last).to_a.each
   end
   def download
     range = target_range()
-    range.each do |r|
-      page = @agent(url + "page-#{start}")
-      puts "page #{start} saved."
-      page.file.save("cache/#{start}.html")
+    range.each do |n|
+      page = @agent.get(URL + "page-#{n}")
+      puts "page #{n} saved."
+      page.save("cache/#{n}.html")
     end
   end
 end
 
 crawl = Crawl.new()
+crawl.download()
