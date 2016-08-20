e549bc351c272add75a50a47aa35725449a7de15
Kiba <hackerkiba@gmail.com>
2016-08-20 18:55:12 -0400

    figure out how to get the time of each post

--- scrape.rb
+++ scrape.rb
@@ -3,4 +3,14 @@ require "nokogiri"
 
 files = Dir["cache/*.html"]
 
+post = {}
+
 content = Nokogiri::HTML(File.open(files[0]))
+
+messages = content.search("li.message")
+
+dates = messages.search("span.DateTime")
+
+dates.each do |d|
+  puts d["title"]
+end
