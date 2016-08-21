2850c5bdb0452451fb244ae5199fae4478a9e6a5
Kiba <hackerkiba@gmail.com>
2016-08-20 21:37:22 -0400

    chang posts from a hash to an array

--- scrape.rb
+++ scrape.rb
@@ -4,18 +4,18 @@ require "byebug"
 
 files = Dir["cache/*.html"]
 
-posts = {}
+posts = []
 
 content = Nokogiri::HTML(File.open(files[0]))
 
 messages = content.search("li.message").each do |m|
-
+  puts "beep"
   metadata = {
     :author => m["data-author"],
     :id => m["id"],
     :date => m.search("div.messageMeta span.DateTime").children.text
   }
-  posts.merge!(metadata)
+  posts.push(metadata)
 end
 
 puts posts
