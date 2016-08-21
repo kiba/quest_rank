6b0802a049d22f1ce81eed082112e0dcf08f57bb
Kiba <hackerkiba@gmail.com>
2016-08-20 22:00:34 -0400

    figured out what to do, an write a json file

--- scrape.rb
+++ scrape.rb
@@ -9,13 +9,18 @@ posts = []
 content = Nokogiri::HTML(File.open(files[0]))
 
 messages = content.search("li.message").each do |m|
-  puts "beep"
-  metadata = {
-    :author => m["data-author"],
-    :id => m["id"],
-    :date => m.search("div.messageMeta span.DateTime").children.text
+  hash = {
+    {
+      :id => m["id"],
+      :author => m["data-author"],
+      :date => m.search("div.messageMeta span.DateTime").children.text
+    }
   }
-  posts.push(metadata)
+  posts.push(hash)
 end
 
-puts posts
+results = JSON.pretty_generate(posts)
+
+File.open("test.json","w") do |f|
+  f.write(posts)
+end
