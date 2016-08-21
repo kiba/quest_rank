430e2fa0b18b53263782733b92ba92ea5f481c00
Kiba <hackerkiba@gmail.com>
2016-08-20 20:03:42 -0400

    begin putting together metadata

--- scrape.rb
+++ scrape.rb
@@ -3,14 +3,17 @@ require "nokogiri"
 
 files = Dir["cache/*.html"]
 
-post = {}
+posts = {}
 
 content = Nokogiri::HTML(File.open(files[0]))
 
-messages = content.search("li.message")
+messages = content.search("li.message").each do |m|
 
-dates = messages.search("div.messageMeta span.DateTime")
-
-dates.each do |d|
-  puts d["title"]
+  metadata = {
+    :author => m.css("username").children.text,
+    :date => m.search("div.messageMeta span.DateTime").children.text
+  }
+  posts.merge!(metadata)
 end
+
+puts posts
