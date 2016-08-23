67888fb183cb9c4764326c0d573b3584b2c15815
Kiba <hackerkiba@gmail.com>
2016-08-23 11:35:55 -0400

    wrote prototype code for counting how many posts for each date.

--- analyze.rb
+++ analyze.rb
@@ -2,4 +2,16 @@ require "json"
 
 file = File.read("doc/data.json")
 
-json = JSON.parse(file)
+data = JSON.parse(file)
+
+dates = {}
+
+element = JSON.parse(data[0])
+
+if dates.key?(element["date"]) == true
+  dates[element["date"]] += 1
+else
+  dates.merge!({element["date"] => 1})
+end
+
+puts dates
