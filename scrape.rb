1d6afa3ae0d5fbe70391eeff4b3c9360243f93f1
Kiba <hackerkiba@gmail.com>
2016-08-23 20:21:48 -0400

    write code to check for regularity

--- scrape.rb
+++ scrape.rb
@@ -10,13 +10,17 @@ files.each do |f|
   content = Nokogiri::HTML(File.open(f))
   replies = content.search("li.message")
   if replies.size != 25
-    puts "Not standard number of replies at... " + f
+    puts "Not standard number of replies at... " + f + " with " + replies.size.to_s
   end
   replies.each do |m|
+    date = m.search("div.messageMeta span.DateTime").children.text
+    if date == ""
+      puts "An empty date in " + f
+    end
     hash = {
       :id => m["id"],
       :author => m["data-author"],
-      :date => m.search("div.messageMeta span.DateTime").children.text
+      :date => date
     }
     posts.push(hash.to_json)
   end
