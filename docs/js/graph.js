91df4d39d3a95ec9c11e8b58f5e13c4deb9f711b
Kiba <hackerkiba@gmail.com>
2016-08-30 17:01:33 -0400

    add line

--- docs/js/graph.js
+++ docs/js/graph.js
@@ -4,6 +4,10 @@ var height = 500;
 var x-axis = d3.scaleTime().range([0,width]);
 var y-axis = d3.scaleTime().range([height,0]);
 
+var line = d3.line()
+.x(function(d) { return x(d[0]);})
+.y(function(d) { return y(d[1]);});
+
 d3.json("date-posts-frequency.json", function (error,data) {
   if (error) { alert(error);}
   for(i=0;i < 30;i++)
