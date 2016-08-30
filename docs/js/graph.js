525320ba660a2477a7566aea45a31afff79c64d3
Kiba <hackerkiba@gmail.com>
2016-08-30 16:59:59 -0400

    define height and width axis

--- docs/js/graph.js
+++ docs/js/graph.js
@@ -1,6 +1,9 @@
 var width = 900;
 var height = 500;
 
+var x-axis = d3.scaleTime().range([0,width]);
+var y-axis = d3.scaleTime().range([height,0]);
+
 d3.json("date-posts-frequency.json", function (error,data) {
   if (error) { alert(error);}
   for(i=0;i < 30;i++)
