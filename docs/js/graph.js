59cfb609f202faa3fd7674f623458a7e124e9dbe
Kiba <hackerkiba@gmail.com>
2016-08-30 17:06:25 -0400

    fix syntax error and append svg

--- docs/js/graph.js
+++ docs/js/graph.js
@@ -1,12 +1,17 @@
 var width = 900;
 var height = 500;
 
-var x-axis = d3.scaleTime().range([0,width]);
-var y-axis = d3.scaleTime().range([height,0]);
+var x_axis = d3.scaleTime().range([0,width]);
+var y_axis = d3.scaleTime().range([height,0]);
 
 var line = d3.line()
 .x(function(d) { return x(d[0]);})
 .y(function(d) { return y(d[1]);});
+
+var svg = d3.select("body").append("svg")
+.attr("width",width + 100)
+.attr("height", height + 100)
+.append("g");
 
 d3.json("date-posts-frequency.json", function (error,data) {
   if (error) { alert(error);}
