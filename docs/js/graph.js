e40fe25abd9d69e1131e4911f7ad98545fa9bffd
Kiba <hackerkiba@gmail.com>
2016-08-31 20:31:31 -0400

    make use of jquery document ready function and indent

--- docs/js/graph.js
+++ docs/js/graph.js
@@ -1,45 +1,48 @@
-var width = 900;
-var height = 500;
+$(document).ready(function()
+{
+  var width = 900;
+  var height = 500;
 
-var x_axis = d3.scaleTime().range([0,width]);
-var y_axis = d3.scaleTime().range([height,0]);
+  var x_axis = d3.scaleTime().range([0,width]);
+  var y_axis = d3.scaleTime().range([height,0]);
 
-var line = d3.line()
-.x(function(d) { return x(d.date); })
-.y(function(d) { return y(d.count); });
+  var line = d3.line()
+  .x(function(d) { return x(d.date); })
+  .y(function(d) { return y(d.count); });
 
-var svg = d3.select("#svg").append("svg")
-.attr("width",width + 100)
-.attr("height", height + 100)
-.append("g");
+  var svg = d3.select("#svg").append("svg")
+  .attr("width",width + 100)
+  .attr("height", height + 100)
+  .append("g");
 
-d3.json("date-posts-frequency.json", function (error,data) {
-  if (error) { alert(error);}
+  d3.json("date-posts-frequency.json", function (error,data) {
+    if (error) { alert(error);}
 
-  var last_thirty = [];
+    var last_thirty = [];
 
-  for(i=0;i < 30;i++)
-  {
-    var d = data[data.length - 30 + i];
-    last_thirty.push({date: d[0],count: d[1]});
-  }
-
-  x_axis.domain(d3.extent(last_thirty,function(d)
+    for(i=0;i < 30;i++)
     {
-      return d.date;
+      var d = data[data.length - 30 + i];
+      last_thirty.push({date: d[0],count: d[1]});
     }
-  ));
-  y_axis.domain(
-    [0, d3.max(last_thirty,function(d) {return d.count;})]
-  );
 
-  svg.append("path")
-  .data([last_thirty])
-  .attr("class","line")
-  .attr("d",line);
-/*
-  last_thirty.forEach(function(obj){
-    console.log(obj);
+    x_axis.domain(d3.extent(last_thirty,function(d)
+      {
+        return d.date;
+      }
+    ));
+    y_axis.domain(
+      [0, d3.max(last_thirty,function(d) {return d.count;})]
+    );
+
+    svg.append("path")
+    .data([last_thirty])
+    .attr("class","line")
+    .attr("d",line);
+  /*
+    last_thirty.forEach(function(obj){
+      console.log(obj);
+    });
+  */
   });
-*/
 });
