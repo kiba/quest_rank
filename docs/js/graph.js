1d87526231fea9e06214a1e7b213db793b07ad8f
Kiba <hackerkiba@gmail.com>
2017-01-24 11:37:09 -0500

    move code around

--- docs/js/graph.js
+++ docs/js/graph.js
@@ -198,9 +198,3 @@ function graphing(error,data) {
     every_month(data,update_draw);
   });
 }
-
-$(document).ready(function()
-{
-  d3.json("data/date-posts-frequency.json", graphing);
-
-});
---
+++ docs/js/main.js
@@ -0,0 +1,5 @@
+$(document).ready(function()
+{
+  d3.json("data/date-posts-frequency.json", graphing);
+
+});
