001e42951be918d1efb169f9d07c904e162d88de
Kiba <hackerkiba@gmail.com>
2017-01-24 13:42:34 -0500

    add link

--- docs/js/metadata.js
+++ docs/js/metadata.js
@@ -3,4 +3,5 @@ function metadata(error,data)
   if (error) { alert(error);}
   $("#title").text(data.title);
   $("#description").text(data.description);
+  $("#link").html("<a href=\"" + data.url +"\">" + "Link" + "</a>");
 }
