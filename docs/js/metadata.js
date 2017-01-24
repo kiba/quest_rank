b72c13612126be2d30e9f4a9d11c7da97d06aeac
Kiba <hackerkiba@gmail.com>
2017-01-24 15:05:01 -0500

    get the list of authors

--- docs/js/metadata.js
+++ docs/js/metadata.js
@@ -2,6 +2,13 @@ function metadata(error,data)
 {
   if (error) { alert(error);}
   $("#title").text(data.title);
+  var result = "Authors: "
+  for (var i= 0;i < data.authors.length;i ++)
+  {
+    result += data.authors[i] + ", ";
+  }
+  result = result.slice(0, -2);
+  $("#authors").text(result);
   $("#description").text(data.description);
   $("#link").html("<a href=\"" + data.url +"\">" + "Link" + "</a>");
 }
