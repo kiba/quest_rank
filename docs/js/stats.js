e2593a4f7d2d5422d28c80b1a50e5a1d9e0f02a2
Kiba <hackerkiba@gmail.com>
2017-01-24 15:12:40 -0500

    add the thirty days average

--- docs/js/stats.js
+++ docs/js/stats.js
@@ -1,3 +1,15 @@
+function thirty_days_average (data)
+{
+  var count = 0;
+  for(i=0;i < 30;i++)
+  {
+    var d = data[data.length - 30 + i];
+    count += d.count;
+  }
+  var average = count / 30;
+  return average;
+}
+
 function stats(error,data)
 {
   if (error) { alert(error);}
