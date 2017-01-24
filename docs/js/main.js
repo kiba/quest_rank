58248bd3b36316b2e76dd177fd42210ce41bb860
Kiba <hackerkiba@gmail.com>
2017-01-24 15:09:50 -0500

    retrieve data for stats function

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function()
 {
   d3.json("data/date-posts-frequency.json", graphing);
+  d3.json("data/date-posts-frequency.json", stats);
   d3.json("data/quest.json",metadata);
 });
