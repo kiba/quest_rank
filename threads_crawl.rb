6bdbcd385e3507b711a0605c0304c08e94d517fd
Kiba <hackerkiba@gmail.com>
2017-01-24 23:38:28 -0500

    get rid of unnecessary code

--- threads_crawl.rb
+++ threads_crawl.rb
@@ -1,34 +1,6 @@
-require "mechanize"
 require "optparse"
 
 URL = "https://forums.sufficientvelocity.com/forums/quests.29/"
-class Crawl
-  def initialize
-    @agent = Mechanize.new()
-    @agent.history_added = Proc.new {sleep 3}
-    @files = Dir["cache/threads/*.html"]
-  end
-  def target_range
-    start = @files.size
-    if start == 0
-      start = 1
-    end
-    page = @agent.get(URL + "page-#{start}")
-    last = page.css("div.PageNav").first["data-last"].to_i
-    (start..last).to_a.each
-  end
-  def auto_download
-    range = target_range()
-    range.each do |n|
-      download(n)
-    end
-  end
-  def download n
-    page = @agent.get(URL + "page-#{n}")
-    puts "page #{n} saved."
-    page.save!("cache/#{n}.html")
-  end
-end
 
 crawl = Crawl.new()
 crawl.auto_download()
