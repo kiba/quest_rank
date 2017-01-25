4e1bb8a1de27633ed4abb1b2b76d60d6d08aa5ad
Kiba <hackerkiba@gmail.com>
2017-01-25 16:56:03 -0500

    add threads_scrape.rb

---
+++ threads_scrape.rb
@@ -0,0 +1,8 @@
+require "json"
+require "nokogiri"
+require "byebug"
+
+files = Dir["cache/threads/*.html"]
+
+# Integer sort.
+files = files.sort_by {|f| f.split("cache/")[1].split(".html").first.to_i}
