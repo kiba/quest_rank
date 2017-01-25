6ae1d7a2f49b8f99d95306ba302faaa45a644309
Kiba <hackerkiba@gmail.com>
2017-01-24 19:22:39 -0500

    append to the ul

--- docs/js/stats.js
+++ docs/js/stats.js
@@ -13,11 +13,16 @@ function average (data, length)
 function stats(error,data)
 {
   if (error) { alert(error);}
+
   var thirty = Math.trunc(average(data,30));
+  $("ul").append("<li>Last Thirty Days Average: " + thirty + " replies.");
+
   if (data.length >= 365)
   {
     var year_on_year = Math.trunc(average(data,365));
+    $("ul").append("<li>Year on Year Average: " + year_on_year + " replies.");
   }
   var lifetime = Math.trunc(average(data,data.length));
-  $("ul").append("<li>Last Thirty Days Average: " + thirty + " replies.");
+
+
 }
