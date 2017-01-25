f45b501b8f5899c04b23ca3d721da79210451171
Kiba <hackerkiba@gmail.com>
2017-01-25 16:57:23 -0500

    open file

--- threads_scrape.rb
+++ threads_scrape.rb
@@ -6,3 +6,7 @@ files = Dir["cache/threads/*.html"]
 
 # Integer sort.
 files = files.sort_by {|f| f.split("cache/")[1].split(".html").first.to_i}
+
+files.each do |f|
+  content = Nokogiri::HTML(File.open(f))
+end
