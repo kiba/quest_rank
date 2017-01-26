01fc299f2ea6c53022d42914301abeaddda30c23
Kiba <hackerkiba@gmail.com>
2017-01-25 20:36:17 -0500

    figure out proper sorting

--- threads_scrape.rb
+++ threads_scrape.rb
@@ -5,8 +5,12 @@ require "byebug"
 files = Dir["cache/threads/*.html"]
 
 # Integer sort.
-files = files.sort_by {|f| f.split("cache/")[1].split(".html").first.to_i}
+files = files.sort_by {|f|
+  f.split("cache/threads/")[1].split(".html").first.to_i
+}
 
 files.each do |f|
   content = Nokogiri::HTML(File.open(f))
+  threads = content.search("li.discussionListItem")
+  byebug
 end
