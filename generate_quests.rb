06da2cea824367e2d24b6cacc11f2adc2814650d
Kiba <hackerkiba@gmail.com>
2017-01-27 14:45:42 -0500

    don't need a list of directory

--- generate_quests.rb
+++ generate_quests.rb
@@ -1,11 +1,8 @@
 require "json"
 require "byebug"
 
-directories = Dir.glob("docs/data/quests/*")
+directory = "docs/data/quests/"
 exclude = []
-directories.each do |d|
-  exclude.push(d.split("/").last)
-end
 threads = JSON.parse(File.read("docs/data/threads.json"))
 
 threads.each do |t|
