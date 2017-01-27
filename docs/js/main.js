8853d3d5af2278f05e881f81b85ba384ff94f904
Kiba <hackerkiba@gmail.com>
2017-01-26 22:43:56 -0500

    get root directory

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function()
 {
   var id = document.location.pathname.split("/quests/")[1].split(".html")[0]
-  var path = "/data/quests/" + id + "/"
+  var path = location.pathname.split("quests/")[0] + "data/quests/" + id + "/"
   d3.json(path + "date-posts-frequency.json", graphing);
   d3.json(path + "date-posts-frequency.json", stats);
   d3.json(path + "quest.json",metadata);
