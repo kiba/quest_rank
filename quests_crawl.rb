10b1fd6d11b0592ebe1e3a66327204d4427f78d1
Kiba <hackerkiba@gmail.com>
2017-01-26 22:51:54 -0500

    get rid of a lot of stuff that's no longer used

--- quests_crawl.rb
+++ quests_crawl.rb
@@ -1,24 +1,4 @@
-require "optparse"
 load "crawl.rb"
 url = "https://forums.sufficientvelocity.com/threads/marked-for-death-a-rational-naruto-quest.24481/"
 
-options = {}
-OptionParser.new do |opts|
-  opts.banner = "Usage: changes.rb [options]"
-  opts.on("-pPAGE", "--page=PAGE", "Choose specificaly which page to download") do |p|
-    options[:page] = p
-  end
-
-  opts.on("-h", "--help", "Print all commands.") do
-    puts opts
-    exit
-  end
-end.parse!
-
 crawl = Crawl.new()
-crawl.set_url(url)
-if options[:page] == nil
-  crawl.auto_download()
-else
-  crawl.download(options[:page])
-end
