e5672f2cdfd8f9bd6647c29b5d1e01317891315c
Kiba <hackerkiba@gmail.com>
2017-01-26 22:17:40 -0500

    adapt for all quests

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,6 +1,8 @@
 $(document).ready(function()
 {
-  d3.json("data/date-posts-frequency.json", graphing);
-  d3.json("data/date-posts-frequency.json", stats);
-  d3.json("data/quest.json",metadata);
+  var id = document.location.pathname.split("/quests/")[1].split(".html")[0]
+  var path = "/data/quests/" + id + "/"
+  d3.json(path + "date-posts-frequency.json", graphing);
+  d3.json(path + "date-posts-frequency.json", stats);
+  d3.json(path + "quest.json",metadata);
 });
