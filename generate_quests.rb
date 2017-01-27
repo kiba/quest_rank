0b1defcca4823459a2611cac699a457ca71d5cd3
Kiba <hackerkiba@gmail.com>
2017-01-27 11:54:01 -0500

    generate a list of excluded stuff

--- generate_quests.rb
+++ generate_quests.rb
@@ -2,4 +2,8 @@ require "json"
 require "byebug"
 
 directories = Dir.glob("docs/data/quests/*")
+exclude = []
+directories.each do |d|
+  exclude.push(d.split("/").last)
+end
 threads = JSON.parse(File.read("docs/data/threads.json"))
