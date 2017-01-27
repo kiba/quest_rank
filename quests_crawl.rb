7949f3ededcb5b68bcb0f3dd51e9b5288c936ace
Kiba <hackerkiba@gmail.com>
2017-01-27 08:22:32 -0500

    get a list of quests

--- quests_crawl.rb
+++ quests_crawl.rb
@@ -1,4 +1,7 @@
 load "crawl.rb"
 url = "https://forums.sufficientvelocity.com/threads/marked-for-death-a-rational-naruto-quest.24481/"
 
+quests = Dir.glob("docs/data/quests/*")
+
+
 crawl = Crawl.new()
