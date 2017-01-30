6d074568ceb453e67137d7fd42b6550a2434cf98
Kiba <hackerkiba@gmail.com>
2017-01-30 16:30:36 -0500

    read file

--- threads_crawl.rb
+++ threads_crawl.rb
@@ -1,6 +1,8 @@
 load "crawl.rb"
 
 file = File.open("progress.txt","r")
+progress = file.read[0].to_i
+file.close()
 
 url = "https://forums.sufficientvelocity.com/forums/quests.29/"
 directory = "threads"
