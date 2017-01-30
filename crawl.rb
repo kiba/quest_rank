321f76d83d4516680048da7596c2c973c6a8fec4
Kiba <hackerkiba@gmail.com>
2017-01-30 16:22:44 -0500

    follow the robots.txt exclusion policy

--- crawl.rb
+++ crawl.rb
@@ -2,6 +2,7 @@ require "mechanize"
 class Crawl
   def initialize
     @agent = Mechanize.new()
+    @agent.robots = true
     @agent.history_added = Proc.new {sleep 6}
     @url = ""
     @target_dir = ""
