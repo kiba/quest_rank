3b1ca3c44f8ca383e018a90d390c44f64a642e05
Kiba <hackerkiba@gmail.com>
2017-04-23 20:26:29 -0400

    encapsulate

--- src/threads_scrape.rb
+++ src/threads_scrape.rb
@@ -10,34 +10,35 @@ def get_quests
   quests
 end
 
-results = []
-files.each do |f|
-  content = Nokogiri::HTML(File.open(f))
-  threads = content.search("li.discussionListItem")
-  threads.each do |t|
-    if t["class"].match("sticky").nil? == true
-      id = t.values[0].split("thread-").last
-      title =  t.search("a").search("a.PreviewTooltip").children.text
-      author = t.values.last
-      create = t.search(".startDate").search(".DateTime").children.text
-      update = t.search(".lastPost").search(".dateTime").search(".DateTime").text
-      tags = []
-      t.search("a.tag").each do |tag|
-        tags.push(tag.values[0].split("/")[1])
+def get_result quest
+  results = []
+  files.each do |f|
+    content = Nokogiri::HTML(File.open(f))
+    threads = content.search("li.discussionListItem")
+    threads.each do |t|
+      if t["class"].match("sticky").nil? == true
+        id = t.values[0].split("thread-").last
+          title =  t.search("a").search("a.PreviewTooltip").children.text
+        author = t.values.last
+        create = t.search(".startDate").search(".DateTime").children.text
+        update = t.search(".lastPost").search(".dateTime").search(".DateTime").text
+        tags = []
+        t.search("a.tag").each do |tag|
+          tags.push(tag.values[0].split("/")[1])
+        end
+        hash = {
+          :id => id,
+          :title => title,
+          :author => author,
+          :create => create,
+          :update => update,
+          :tags => tags
+        }
+        results.push(hash)
       end
-      hash = {
-        :id => id,
-        :title => title,
-        :author => author,
-        :create => create,
-        :update => update,
-        :tags => tags
-      }
-      results.push(hash)
     end
   end
 end
-
 data = JSON.pretty_generate(results)
 File.open("docs/data/threads.json","w") do |f|
   f.write(data)
