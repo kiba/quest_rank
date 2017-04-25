90a615d9f506dedb4acd298ba8b9e4b53ad84d2e
Kiba <hackerkiba@gmail.com>
2017-04-24 20:02:19 -0400

    get rid of crufts.

--- src/threads_scrape.rb
+++ src/threads_scrape.rb
@@ -16,28 +16,24 @@ def get_metadata quest
   content = Nokogiri::HTML(File.open(filename))
   byebug
   document = content.search("li.discussionListItem")
-  threads.each do |t|
-    if t["class"].match("sticky").nil? == true
-      id = t.values[0].split("thread-").last
-      title =  t.search("a").search("a.PreviewTooltip").children.text
-      author = t.values.last
-      create = t.search(".startDate").search(".DateTime").children.text
-      update = t.search(".lastPost").search(".dateTime").search(".DateTime").text
-      tags = []
-      t.search("a.tag").each do |tag|
-        tags.push(tag.values[0].split("/")[1])
-      end
-      hash = {
-        :id => id,
-        :title => title,
-        :author => author,
-        :create => create,
-        :update => update,
-        :tags => tags
-      }
-      return hash
-    end
+  id = t.values[0].split("thread-").las
+  title =  t.search("a").search("a.PreviewTooltip").children.text
+  author = t.values.last
+  create = t.search(".startDate").search(".DateTime").children.text
+  update = t.search(".lastPost").search(".dateTime").search(".DateTime").text
+  tags = []
+  t.search("a.tag").each do |tag|
+    tags.push(tag.values[0].split("/")[1])
   end
+  hash = {
+    :id => id,
+    :title => title,
+    :author => author,
+    :create => create,
+    :update => update,
+    :tags => tags
+  }
+  return hash
 end
 
 quests = get_quests()
